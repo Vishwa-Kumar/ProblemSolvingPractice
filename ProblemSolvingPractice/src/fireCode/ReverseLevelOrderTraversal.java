1837cf3e60d6165725ed74fb93aebce909d6744d
KITSPL\kh2212 <vishwa.deepak@kony.com>
2018-01-06 00:09:48 +0530

    formatting

--- ProblemSolvingPractice/src/fireCode/ReverseLevelOrderTraversal.java
+++ ProblemSolvingPractice/src/fireCode/ReverseLevelOrderTraversal.java
@@ -18,6 +18,7 @@ public class ReverseLevelOrderTraversal {
 			this.right = right;
 		}
 	}
+
 	public static void main(String[] args) {
 		ReverseLevelOrderTraversal.TreeNode root = new ReverseLevelOrderTraversal.TreeNode(1, null, null);
 		root.left = new TreeNode(2, null, null);
@@ -26,50 +27,44 @@ public class ReverseLevelOrderTraversal {
 		root.left.right = new TreeNode(5, null, null);
 		root.right.left = new TreeNode(6, null, null);
 		root.right.right = new TreeNode(7, null, null);
-		
+
 		System.out.println("ans" + new ReverseLevelOrderTraversal().levelorderRev(root));
 
 	}
-	
+
 	public ArrayList<Integer> levelorderRev(TreeNode root) {
-		ArrayList<Integer> returnList=new ArrayList<>();
-		if(root==null)
-		{
+		ArrayList<Integer> returnList = new ArrayList<>();
+		if (root == null) {
 			return returnList;
 		}
-		
-		Queue<TreeNode> q=new LinkedList<TreeNode>();
+
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
 		q.add(root);
-		
-		while(true)
-		{
-			int nodeCount=q.size();
+
+		while (true) {
+			int nodeCount = q.size();
 			TreeNode node;
-			
-			if(nodeCount==0)
+
+			if (nodeCount == 0)
 				break;
-			
-			while(nodeCount>0) {
-				
-				node=q.peek();
+
+			while (nodeCount > 0) {
+
+				node = q.peek();
 				q.remove();
 				returnList.add(node.data);
-				
-				if(node.right!=null)
-				{
+
+				if (node.right != null) {
 					q.add(node.right);
 				}
-				if(node.left!=null)
-				{
+				if (node.left != null) {
 					q.add(node.left);
 				}
 				nodeCount--;
 			}
-		} 
+		}
 		Collections.reverse(returnList);
 		return returnList;
 	}
 
-	
-	
 }
