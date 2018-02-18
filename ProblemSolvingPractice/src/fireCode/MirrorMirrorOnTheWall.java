7dcefc9330e7b66d0dbbc5cecd303906aef1f2b4
KITSPL\kh2212 <vishwa.deepak@kony.com>
2018-02-18 18:29:46 +0530

    Mirror Tree Checking

--- ProblemSolvingPractice/src/fireCode/MirrorMirrorOnTheWall.java
+++ ProblemSolvingPractice/src/fireCode/MirrorMirrorOnTheWall.java
@@ -1,7 +1,5 @@
 package fireCode;
 
-
-
 public class MirrorMirrorOnTheWall {
 
 	private static class TreeNode {
@@ -22,29 +20,25 @@ public class MirrorMirrorOnTheWall {
 		root1.right = new TreeNode(2, null, null);
 		root1.right.left = new TreeNode(5, null, null);
 		root1.right.right = new TreeNode(4, null, null);
-		
-		
-		
-		
+
 		MirrorMirrorOnTheWall.TreeNode root2 = new MirrorMirrorOnTheWall.TreeNode(1, null, null);
 		root2.left = new TreeNode(2, null, null);
 		root2.right = new TreeNode(3, null, null);
 		root2.left.left = new TreeNode(4, null, null);
 		root2.left.right = new TreeNode(5, null, null);
-		System.out.println("ans" + new MirrorMirrorOnTheWall().isMirror(root1,root2));
+		System.out.println("ans" + new MirrorMirrorOnTheWall().isMirror(root1, root2));
 
 	}
+
 	public boolean isMirror(TreeNode root1, TreeNode root2) {
-		
-		if(root1==null && root2==null)
-		{
+
+		if (root1 == null && root2 == null) {
 			return true;
 		}
-		if(root1==null || root2==null)
-		{
+		if (root1 == null || root2 == null) {
 			return false;
 		}
-		return (root1.data==root2.data) && (isMirror(root1.left,root2.right) && (isMirror(root1.right,root2.left)));
+		return (root1.data == root2.data) && (isMirror(root1.left, root2.right) && (isMirror(root1.right, root2.left)));
 	}
 
 }
