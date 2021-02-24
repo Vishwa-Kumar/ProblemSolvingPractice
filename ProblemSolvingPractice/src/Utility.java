7bc2a49802fe0b690e052b1620fd862a80d8f4e1
Vishwa Deepak <videepak@microsoft.com>
2021-02-24 08:50:56 +0530

    search in sorted array

---
+++ ProblemSolvingPractice/src/Utility.java
@@ -0,0 +1,16 @@
+
+public class Utility {
+	
+	public static void printMatrix(int mat[][])
+	{
+		for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				System.out.print(mat[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
